10f7e35474cdc483b782bc03717b7238b6c1c344
alpaca0813 <44792866+alpaca0813@users.noreply.github.com>
2018-12-31 22:27:13 +0900

    Add files via upload

--- alpaca0813.js
+++ alpaca0813.js
@@ -1,4 +1,15 @@
 function jump(){
+
   if (confirm("このページに飛びますか？")==true)
-    location.href = "web.sfc.ac.jp/~s17574yh/info2/info2.html";
+    location.href = "web.sfc.ac.jp/~s17574yh/info2/info2_1.html";
+}
+
+function flashback(){
+  if (confirm("このページに飛びますか？")==true)
+    location.href = "web.sfc.ac.jp/~s17574yh/info2/info2_2.html";
+}
+
+function pop(){
+  if (confirm("このページに飛びますか？")==true)
+    location.href = "https://github.com/alpaca0813/alpaca0813.github.io";
 }
